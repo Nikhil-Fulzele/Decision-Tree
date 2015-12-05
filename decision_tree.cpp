7fd592f334c0133abec4cf27c517142112bb5dda
Nikhil-Fulzele <nikhilf99@gmail.com>
2015-12-05 14:51:58 +0530

    neumeric data and categorical data identification method added

--- decision_tree.cpp
+++ decision_tree.cpp
@@ -3,11 +3,14 @@
 #include<fstream>
 #include<string>
 #include<vector>
+#include<set>
 #include<sstream>
 #include<algorithm>
 #include<random>
 #include<cmath>
 #include<ctime>
+#include<cstdlib>
+#include<cstdio>
 
 using namespace std;
 
@@ -27,9 +30,13 @@ struct node{
 // Logic: if the attribute_value is integer and number of unique attribute_values is more than some threshold then it is neumeric data
 bool is_neumeric(vector<string> attribute_values,long deciding_threshold){
     set<string> values;
-    for(long i; i < attribute_values.size();i++){
+    for(long i = 0; i < attribute_values.size();i++){
         values.insert(attribute_values[i]);
+        if(values.size() > deciding_threshold){
+            return true;
+        }
     }
+    return false;
 }
 
 //Method for spliting the line in vector of attributes
@@ -45,21 +52,37 @@ vector<string> split(string str, char delimiter) {
     return row_attributes_values;
 }
 
+// Method for generating transpose of a 2D vector assuming it is a square 2d vector
+vector<vector<string>> get_transpose_vector(vector<vector<string>> input_2d_vector){
+    long row_size = input_2d_vector.size();
+    long col_size = input_2d_vector[0].size();
+
+    vector<vector<string>> transpose_vector(col_size,vector<string>(row_size));
+    for(long i=0;i<row_size; i++) {
+        for (long j=0; j<col_size; j++){
+	        transpose_vector[j][i] = input_2d_vector[i][j];
+        }
+    }
+    return transpose_vector;
+}
+
 // Method for generating feature vector for training set
 vector<vector<string>> generate_feature_vector(){
-    vector<vector<string>> final_feature_vector;
+    vector<vector<string>> feature_vector, transpose_feature_vector;
     string  file_name = "training.data";
     ifstream training_file(file_name);
     string line;
     if(training_file.is_open())
     {
         while (getline(training_file, line))
-            final_feature_vector.push_back(split(line,','));
+            feature_vector.push_back(split(line,','));
         training_file.close();
     }
     else
         cout << "Unable to open file";
-    return final_feature_vector;
+
+    transpose_feature_vector = get_transpose_vector(feature_vector);
+    return transpose_feature_vector;
 }
 
 //Generate Training set and Testing set and save them to training.data and testing.data
@@ -111,20 +134,24 @@ int main () {
     vector<vector<string>> feature_vector;
 
     //Reading the dataset file name
-    cout << "Enter the file name : ";
-    cin >> file_name;
-
+    //cout << "Enter the file name : ";
+    //cin >> file_name;
+    file_name = "dataset.data";
     //Calling split_dataset to generate training and testing files
     split_dataset(file_name);
 
     //generating feature vector for testing data
     feature_vector = generate_feature_vector();
-    for (int i = 0; i < feature_vector.size(); i++){
-        for (int j = 0; j < feature_vector[j].size(); j++){
-            cout << feature_vector[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
+//    for (int i = 0; i < feature_vector.size(); i++){
+//        for (int j = 0; j < feature_vector[i].size(); j++){
+//            string str = feature_vector[i][j];
+//            str.erase(remove(str.begin(), str.end(), ' '),str.end());
+//            cout << str << ",";
+//        }
+//        cout << "\n";
+//    }
+//    for (int i = 0; i < feature_vector.size(); i++){
+//            cout << (is_neumeric(feature_vector[i],10)? "Yes" : "No");
+//    }
     return 0;
 }
