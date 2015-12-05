7e85c798115db1214f13dc583d4ee933f0eba2cd
Nikhil-Fulzele <nikhilf99@gmail.com>
2015-12-05 19:34:09 +0530

    Gain Ratio calculation added

--- decision_tree.cpp
+++ decision_tree.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include<vector>
 #include<set>
+#include <map>
 #include<sstream>
 #include<algorithm>
 #include<random>
@@ -26,8 +27,86 @@ struct node{
     vector<struct node*> child;
 }tree_node;
 
+// Method for calculating the freq(Ci,S)
+double calculate_freq(vector<string> attribute_classes, string check_class){
+    double freq = 0;
+//    cout << "Third" << "\n";
+    for(long i = 0; i < attribute_classes.size(); i++){
+        if(attribute_classes[i] == check_class){
+            freq += 1;
+        }
+    }
+    return freq;
+}
+
+// Method for calculating information conveyed by complete attrubute set : info(T)
+double cal_info_convey_given_set(vector<string> attribute_classes, double attribute_size){
+    double info_conveyed = 0, freq = 0, freq_ratio = 0;
+    set<string> unique_classes;
+    set<string>::iterator it;
+    cout << "attribute_size : " <<attribute_size<<"\n";
+    //Generating unique classes
+    for(long i = 0; i < attribute_size; i++){
+        unique_classes.insert(attribute_classes[i]);
+    }
+    for(it = unique_classes.begin(); it != unique_classes.end(); it++){
+        //cout << *it << "\n";
+        freq = calculate_freq(attribute_classes,*it);
+        cout << "freq : " << freq << "\n";
+        freq_ratio = freq/attribute_size;
+        cout << "freq_ratio : " << freq_ratio << "\n";
+        info_conveyed += (freq_ratio*log2(freq_ratio));
+        //cout << freq << " : " << freq_ratio << " : "  << info_conveyed << "\n";
+    }
+    info_conveyed = -info_conveyed;
+    return info_conveyed;
+}
+
+// Method for calculating information conveyed by this attribute set : info_x(T)
+vector<double> cal_info_convey_this_attribut_set(vector<string> attribute_classes, vector<string> attribute_values){
+    double info_conveyed = 0;
+    double split_info = 0;
+    double attri_ratio = 0;
+    double attribute_size = attribute_values.size();
+    vector<double> info_and_split;
+
+    map<string, vector<string>> split_category;
+    map<string, vector<string>>::iterator it;
+    for(long i = 0; i < attribute_values.size(); i++){
+        split_category[attribute_values[i]].push_back(attribute_classes[i]);
+    }
+    for(it = split_category.begin(); it != split_category.end(); ++it){
+        attri_ratio = it->second.size()/attribute_size;
+        cout << "it->second.size() : " << it->second.size() << "attribute_size : " << attribute_size <<"\n";
+        info_conveyed += (attri_ratio*cal_info_convey_given_set(it->second,it->second.size()));
+        split_info += (attri_ratio*log2(attri_ratio));
+    }
+    split_info = -split_info;
+    info_and_split.push_back(info_conveyed);
+    info_and_split.push_back(split_info);
+
+    return info_and_split;
+}
+
+// Method to calculate gain ratio
+double get_gain_ratio(vector<string> attribute_classes, vector<string> attribute_values){
+    double gain = 0, gain_ratio = 0, info = 0, info_x = 0, split_info;
+    vector<double> store_info_x;
+    info = cal_info_convey_given_set(attribute_classes,attribute_values.size());
+    cout << "INFO : " << info << "\n";
+    store_info_x = cal_info_convey_this_attribut_set(attribute_classes,attribute_values);
+    info_x = store_info_x[0];
+    cout << "INFO_X : " << info_x << "\n";
+    gain = info - info_x;
+    cout << "gain : " << gain << "\n";
+    split_info = store_info_x[1];
+    cout << "split_info : " << split_info << "\n";
+    gain_ratio = gain/split_info;
+    return gain_ratio;
+}
+
 //Method to differentiate categorical data from neumeric data
-// Logic: if the attribute_value is integer and number of unique attribute_values is more than some threshold then it is neumeric data
+// Logic: If the number of unique attribute_values is more than some threshold then it is neumeric data
 bool is_neumeric(vector<string> attribute_values,long deciding_threshold){
     set<string> values;
     for(long i = 0; i < attribute_values.size();i++){
@@ -69,7 +148,7 @@ vector<vector<string>> get_transpose_vector(vector<vector<string>> input_2d_vect
 // Method for generating feature vector for training set
 vector<vector<string>> generate_feature_vector(){
     vector<vector<string>> feature_vector, transpose_feature_vector;
-    string  file_name = "training.data";
+    string  file_name = "play_dataset.data";
     ifstream training_file(file_name);
     string line;
     if(training_file.is_open())
@@ -132,6 +211,7 @@ void split_dataset(string file_name){
 int main () {
     string file_name; //name of the input file (dataset)
     vector<vector<string>> feature_vector;
+    double feature_size = 0;
 
     //Reading the dataset file name
     //cout << "Enter the file name : ";
@@ -153,5 +233,11 @@ int main () {
 //    for (int i = 0; i < feature_vector.size(); i++){
 //            cout << (is_neumeric(feature_vector[i],10)? "Yes" : "No");
 //    }
+    double feature_col_size = feature_vector.size();
+    double feature_row_size = feature_vector[0].size();
+    for(int i = 0; i < feature_row_size; i ++){
+        //cout << feature_vector[feature_col_size-1][i] << "\n";
+    }
+    cout << get_gain_ratio(feature_vector[feature_col_size-1],feature_vector[0]);
     return 0;
 }
